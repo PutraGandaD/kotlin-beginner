c27894f124fd3ab0ec3eae2a17e1d603f1572b90
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-05-12 16:07:18 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+// main function (single line comment)
+/*
+    multi line comment
+    Hello Kotlin
+ */
+fun main() {
+    println("Hello Kotlin!")
+
+    val name = "Putra Ganda Dewata"
+
+    print("Hello, my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+
